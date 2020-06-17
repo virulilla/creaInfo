c27fed458b6eddf108ea29dafa1eefd62fed0e95
virulilla <e.salido.castro@gmail.com>
2020-06-17 19:17:53 +0200

    Se crea script con funcionalidad completa

---
+++ main.py
@@ -0,0 +1,31 @@
+import arcpy, os
+
+path = "C://SyK//06_GENERAINFO//data"
+mdb = "Frecuencias_Model_def_esc.mdb"
+table = "Frecuencias_INS_160_export"
+txt = "frec_interurbanos.txt"
+
+f = open(os.path.join(path, txt), 'w')
+
+fieldList = ["CODITIN", "HORA1", "FREQ1", "HORA2", "FREQ2", "HORA3", "FREQ3", "HORA4", "FREQ4", "HORA5", "FREQ5",
+             "HORA6", "FREQ6", "HORA7", "FREQ7", "HORA8", "FREQ8", "HORA9", "FREQ9", "HORA10", "FREQ10", "HORA11",
+             "FREQ11", "HORA12", "FREQ12", "HORA13", "FREQ13", "HORA14", "FREQ14", "HORA15", "FREQ15"]
+
+with arcpy.da.SearchCursor(os.path.join(path, mdb, table), fieldList, sql_clause=(None, "ORDER BY CODITIN")) as cursor:
+    for r in cursor:
+        i = 0
+        line = []
+        if i == 0:
+            line.append(str(int(r[i])))
+            line.append(", ")
+            i += 1
+        if i == 1:
+            while i < 31:
+                line.append(str(int(r[i])))
+                line.append(", ")
+                line.append(str(r[i+1]))
+                line.append(", ")
+                i = i + 2
+        line.append("\n")
+        f.writelines(line)
+f.close()
